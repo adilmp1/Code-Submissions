1b8d59f5c5d3f3206cde04cdc2ba4730a6862c33
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-03-15 21:39:00 +0530

    Create Largest-Element-in-Array.cpp

---
+++ Largest-Element-in-Array.cpp
@@ -0,0 +1,12 @@
+int largest(vector<int> &a, int n)
+{
+    int largest = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > largest)
+        {
+            largest = a[i];
+        }
+    }
+    return largest;
+}
