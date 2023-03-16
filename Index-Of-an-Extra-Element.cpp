e707bd76aa6a2cfeeb354e71be95d44c25c3a30d
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-03-16 15:28:12 +0530

    Create Index-Of-an-Extra-Element.cpp

---
+++ Index-Of-an-Extra-Element.cpp
@@ -0,0 +1,11 @@
+int findExtra(int a[], int b[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] != b[i])
+        {
+            return i;
+        }
+    }
+    return n - 1;
+}
