e059cc419e87ddd540071cd2f1745c00252c470e
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-03-16 15:30:36 +0530

    Create Non-Repeating-Element.cpp

---
+++ Non-Repeating-Element.cpp
@@ -0,0 +1,16 @@
+int firstNonRepeating(int a[], int n)
+{
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        mp[a[i]]++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (mp[a[i]] == 1)
+        {
+            return a[i];
+        }
+    }
+    return 0;
+}
