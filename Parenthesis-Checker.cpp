b62faf8007a8bb15d49ccfe1040cd83daef68775
adil <adilmovies7@gmail.com>
2023-06-28 13:57:47 +0530

    commit

---
+++ Parenthesis-Checker.cpp
@@ -0,0 +1,71 @@
+bool ispar(string x)
+{
+    if(x.length()%2==1)
+    {
+        return false;
+    }
+    stack<char> pars;   
+    for (char ch : x)
+    {
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            pars.push(ch);
+        }
+        if (ch == ')')
+        {
+            if(pars.empty())
+            {
+                return false;
+            }
+            while(pars.top()!='(' && !pars.empty()){
+                pars.pop();
+            if(pars.empty())
+            {
+                return false;
+            }
+            }
+            if(pars.top()=='(')
+            {
+                pars.pop();
+            }
+
+        }
+        if (ch == ']')
+        {
+            if(pars.empty())
+            {
+                return false;
+            }
+            while(pars.top()!='[' && !pars.empty()){
+                pars.pop();
+                if(pars.empty())
+                {
+                    return false;
+                }
+            }
+            if(pars.top()=='[')
+            {
+                pars.pop();
+            }
+        }
+        if (ch == '}')
+        {
+            if(pars.empty())
+            {
+                return false;
+            }
+            while(pars.top()!='{' && !pars.empty()){
+                pars.pop();
+                if(pars.empty())
+                {
+                    return false;
+                }
+            }
+            if(pars.top()=='{')
+            {
+                pars.pop();
+            }
+        }
+    }
+    return pars.empty();
+}
