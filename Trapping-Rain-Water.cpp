57dc5f056b8b16c54ef0c5a8bad2f9468c017af1
adil <adilmovies7@gmail.com>
2023-06-30 21:49:44 +0530

    trapping rain water

---
+++ Trapping-Rain-Water.cpp
@@ -0,0 +1,21 @@
+long long trappingWater(int arr[], int n)
+{
+    long long answer=0;
+    int l[n],r[n];
+    l[0]=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        l[i] = max(l[i-1],arr[i]);
+    }
+    r[n-1]=arr[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+        r[i] = max(r[i+1],arr[i]);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        answer+=max(0,min(l[i],r[i])-arr[i]);
+    }
+    return answer;
+}
