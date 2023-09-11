3f2113c5dba7221f46edd080a4a051e887eddbd3
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-09-11 20:22:36 +0530

    Create Queue-using-two-Stacks.cpp

---
+++ Queue-using-two-Stacks.cpp
@@ -0,0 +1,22 @@
+void StackQueue ::push(int x)
+{
+    s1.push(x);
+}
+int StackQueue ::pop()
+{
+    if (s1.empty())
+        return -1;
+    while (!s1.empty())
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+    int val = s2.top();
+    s2.pop();
+    while (!s2.empty())
+    {
+        s1.push(s2.top());
+        s2.pop();
+    }
+    return val;
+}
