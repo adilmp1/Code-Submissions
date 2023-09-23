45d1be4706ad543bc1eacf902d0b4b18802ca69f
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-09-23 10:14:04 +0530

    Update Reverse-a-Stack.cpp

--- Reverse-a-Stack.cpp
+++ Reverse-a-Stack.cpp
@@ -1,3 +1,31 @@
+// code 1 with O(1) space complexity
+void insertAtBottom(stack<int>&s,int val)
+{
+    if(s.empty())
+    {
+        s.push(val);
+    }
+    else
+    {
+        int x = s.top();
+        s.pop();
+        insertAtBottom(s,val);
+        s.push(x);
+    }
+}
+void Reverse(stack<int> &s)
+{
+    int val = s.top();
+    s.pop();
+    if (!s.empty())
+    {
+        Reverse(s);
+    }
+    insertAtBottom(s,val);
+}
+
+
+// code 2 with O(n) space complexity
 vector<int> temp;
 void Reverse(stack<int> &s)
 {
