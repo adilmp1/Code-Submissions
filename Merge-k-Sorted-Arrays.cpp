cb84e58d0bfddc8c35f262f57b0dfc8177b0b5c9
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-09-24 20:02:35 +0530

    Update Merge-k-Sorted-Arrays.cpp

--- Merge-k-Sorted-Arrays.cpp
+++ Merge-k-Sorted-Arrays.cpp
@@ -11,3 +11,51 @@ vector<int> mergeKArrays(vector<vector<int>> a, int K)
     sort(ans.begin(), ans.end());
     return ans;
 }
+
+//Optimized
+
+class data{
+    public:
+    int val,apos,vpos;
+    data(int v,int ap,int vp)
+    {
+        val=v;
+        apos=ap;
+        vpos=vp;
+    }
+};
+struct mycomp
+{
+    bool operator()(data& d1,data& d2)
+    {
+        return d1.val > d2.val;
+    }
+};
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> a, int k)
+    { 
+        vector<int> ans;
+        priority_queue<data,vector<data>,mycomp> pq;
+        for(int i=0;i<k;i++)
+        {
+            data d(a[i][0],i,0);
+            pq.push(d);
+        }
+        while(!pq.empty())
+        {
+            data temp = pq.top();
+            ans.push_back(temp.val);
+            pq.pop();
+            if(temp.vpos!=k-1)
+            {
+                temp.vpos++;
+                temp.val = a[temp.apos][temp.vpos];
+                pq.push(temp);
+            }
+        }
+        return ans;
+    }
+};
