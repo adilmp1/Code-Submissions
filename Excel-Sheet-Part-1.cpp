6a17c69f479f8c9b2889ab62545cb0cb6f741047
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-10-15 21:53:12 +0530

    Create Excel-Sheet-Part-1.cpp

---
+++ Excel-Sheet-Part-1.cpp
@@ -0,0 +1,34 @@
+    string ExcelColumn(int n)
+    {
+        string ans;
+        int mode = n % 26;
+        if (mode == 0)
+        {
+            ans.insert(0, 1, 'Z');
+            n -= 26;
+        }
+        else if (mode == 26)
+        {
+            n -= 26;
+        }
+        else
+        {
+            n = n - n % 26;
+        }
+        if (mode)
+        {
+            char temp = 65 + mode - 1;
+            ans.insert(0, 1, temp);
+        }
+        while (n)
+        {
+            n /= 26;
+            mode = n % 26;
+            if (mode == 0)
+            {
+                ans.insert(0, 1, 'Z');
+                n -= 26;
+            }
+            else if (mode == 26)
+            {
+                n -= 26;
