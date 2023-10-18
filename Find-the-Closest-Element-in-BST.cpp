a6322532fcaed6ded8d25a93a5e47ebcca6b13a1
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-10-18 16:43:21 +0530

    Update Find-the-Closest-Element-in-BST.cpp

--- Find-the-Closest-Element-in-BST.cpp
+++ Find-the-Closest-Element-in-BST.cpp
@@ -14,3 +14,28 @@ int minDiff(Node *root, int k)
     helper(root, k);
     return ans;
 }
+
+
+//Optimized code
+int ans = INT_MAX;
+void helper(Node *root, int k)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    ans = min(ans, abs(k - root->data));
+    if (root->data < k)
+    {
+        helper(root->right, k);
+    }
+    else
+    {
+        helper(root->left, k);
+    }
+}
+int minDiff(Node *root, int k)
+{
+    helper(root, k);
+    return ans;
+}
