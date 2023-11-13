3d5f5ab439a645b47f0e9eea3a289465dfa4cead
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-11-13 17:45:17 +0530

    Create Shortest-Common-Supersequence.cpp

---
+++ Shortest-Common-Supersequence.cpp
@@ -0,0 +1,19 @@
+    int shortestCommonSupersequence(string x, string y, int m, int n)
+    {
+        vector<vector<int>> dp (m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(x[i-1]==y[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return m+n-dp[m][n];
+    }
