b83fcabc9dd85077944061d436100bf468a9a0df
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-11-27 19:36:18 +0530

    Update DFS-of-Graph.cpp

--- DFS-of-Graph.cpp
+++ DFS-of-Graph.cpp
@@ -1,4 +1,29 @@
-    unordered_map<int,int> mp;
+// Optimized using boolean array for storing visited nodes
+    vector<int> ans;
+    void dfs(int idx,vector<int> adj[],bool visited[])
+    {
+        if(visited[idx])
+        {
+            return;
+        }
+        visited[idx]=true;
+        ans.push_back(idx);
+        for(int i:adj[idx])
+        {
+            dfs(i,adj,visited);
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) 
+    {
+        bool visited[V]={false};
+        dfs(0,adj,visited); 
+        return ans;
+    }
+
+// hashmap is used to keep track of visited nodes
+
+
+unordered_map<int,int> mp;
     vector<int> ans;
     void dfs(int idx,vector<int> adj[])
     {
