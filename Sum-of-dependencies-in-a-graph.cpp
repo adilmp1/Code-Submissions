18aa2a17e1d14d640b5a925ff043e836b3cced19
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2023-11-28 08:00:28 +0530

    Create Sum-of-dependencies-in-a-graph.cpp

---
+++ Sum-of-dependencies-in-a-graph.cpp
@@ -0,0 +1,9 @@
+    int sumOfDependencies(vector<int> adj[], int V) {
+        int ans=0;
+        for(int i=0;i<V;i++)
+        {
+            ans+=adj[i].size();
+        }
+        return ans;
+        // code here
+    }
