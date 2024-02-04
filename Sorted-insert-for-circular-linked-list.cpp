18d453286799bb457b76eb061127fb8b75f17d66
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2024-02-05 05:15:48 +0530

    Update Sorted-insert-for-circular-linked-list.cpp

--- Sorted-insert-for-circular-linked-list.cpp
+++ Sorted-insert-for-circular-linked-list.cpp
@@ -1,30 +1,32 @@
-Node *sortedInsert(Node *head, int data)
-{
-    Node *newNode = new Node(data);
-    Node *var;
-    Node *ptr = head;
-    if (head->data > data)
-    {
-        newNode->next = head;
-        for (var = head; var->next != head; var = var->next)
-            ;
-        head = newNode;
-        var->next = head;
+        Node* newNode = new Node(data);
+        if(head==NULL)
+        {
+            newNode->next=newNode;
+            head=newNode;
+            return head;
+        }     
+        Node *tail;
+        for(tail=head;tail->next!=head;tail=tail->next);
+        Node *ptr = head;
+        if (head->data >= data)
+        {
+            newNode->next = head;
+            tail->next=newNode;
+            head = newNode;
+            return head;
+        }
+        Node *temp;
+        while (ptr->data <= data && ptr->next != head)
+        {
+            temp = ptr;
+            ptr = ptr->next;
+        }
+        if (ptr->next == head && ptr->data < data)
+        {
+            ptr->next = newNode;
+            newNode->next=head;
+            return head;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
         return head;
-    }
-    Node *temp;
-    while (ptr->data <= data && ptr->next != head)
-    {
-        temp = ptr;
-        ptr = ptr->next;
-    }
-    if (ptr->next == head && ptr->data < data)
-    {
-        ptr->next = newNode;
-        newNode->next = head;
-        return head;
-    }
-    newNode->next = temp->next;
-    temp->next = newNode;
-    return head;
-}
