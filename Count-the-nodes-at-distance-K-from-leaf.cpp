d9b85294950f8a806b6b30e3f2bdff2a9e91425e
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2024-02-07 06:31:49 +0530

    Update Count-the-nodes-at-distance-K-from-leaf.cpp

--- Count-the-nodes-at-distance-K-from-leaf.cpp
+++ Count-the-nodes-at-distance-K-from-leaf.cpp
@@ -1,3 +1,25 @@
+// My Code
+    bool isHeightK(Node * root,int k)
+    {
+        if(root==NULL) return false;
+        if(root->left==NULL and root->right==NULL and k==0) return true;
+        return (isHeightK(root->left,k-1)||isHeightK(root->right,k-1));
+    }
+    int count(Node * root, int k)
+    {
+        if(root==NULL) return 0;
+        if(isHeightK(root,k))
+        {
+            return (1+count(root->left,k)+count(root->right,k));
+        }
+        return (count(root->left,k)+count(root->right,k));
+    }
+    int printKDistantfromLeaf(Node* root, int k){
+        return count(root, k);
+    }
+
+
+// Copied Code
     bool heightK(Node* node, int k){
         if(!node){
             return false;
