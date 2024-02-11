d35a51b7acc1580b13ba148bdf43c65c996681fc
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2024-02-11 16:36:40 +0530

    Update Number-of-paths-in-a-matrix-with-k-coins.cpp

--- Number-of-paths-in-a-matrix-with-k-coins.cpp
+++ Number-of-paths-in-a-matrix-with-k-coins.cpp
@@ -1,3 +1,18 @@
+    //DP Code
+    int helper(int row,int col,int n,int k,vector<vector<int>>& a,vector<vector<vector<long long>>> & dp)
+    {
+        if(row>=n or col>=n) return 0;
+        if(k<0) return 0;
+        if(dp[row][col][k]!=-1) return dp[row][col][k];
+        if(row==n-1 and col==n-1 and k==a[row][col]) return (dp[row][col][k]=1);
+        return (dp[row][col][k]=(helper(row+1,col,n,k-a[row][col],a,dp)+helper(row,col+1,n,k-a[row][col],a,dp)));
+        
+    }
+    long long numberOfPath(int n, int k, vector<vector<int>> &a)
+    {
+        vector<vector<vector<long long>>> dp(n,vector<vector<long long>>(n,vector<long long>(k+1,-1)));
+        return helper(0,0,n,k,a,dp);
+    }
 
     // recursive code 
     void helper(int row,int col,int n,int k,vector<vector<int>>& a,long long & ans)
